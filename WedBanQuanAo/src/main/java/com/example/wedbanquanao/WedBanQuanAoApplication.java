4bcb6cbeaa4dd4f2d202d8738077280a092f9153
Vietanh1311 <nva0372206089@gmail.com>
2022-11-18 09:44:57 +0700

    start

---
+++ WedBanQuanAo/src/main/java/com/example/wedbanquanao/WedBanQuanAoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedbanquanao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedBanQuanAoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedBanQuanAoApplication.class, args);
+	}
+
+}
